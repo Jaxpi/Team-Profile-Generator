2c0e3034b585c0456d6d8fa599a2c50b41445bf8
Jax I <jaxpi85@gmail.com>
2022-10-25 19:54:50 -0400

    removed husky since I don't know what it does and npm start isn't working

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const Manager = require("./lib/Manager.js");
 // Team member data
 const myTeam = [];
 
-const employeeQuestions = [
+// These are the prompts for the user
+const employeePrompts = [
     {
         type: "input",
         name: "name",
@@ -24,15 +25,9 @@ const employeeQuestions = [
         name: "email",
         message: "Enter Employee Email Address"
     },
-    {
-        type: "list",
-        name: "role",
-        message: "Choose Employee Role",
-        choices: ["Engineer", "Intern", "Manager"]
-    }
 ]
 
-const engineerQuestions = [
+const engineerPrompt = [
     {
         type: "input",
         name: "github",
@@ -40,7 +35,7 @@ const engineerQuestions = [
     }
 ]
 
-const internQuestions = [
+const internPrompt = [
     {
         type: "input",
         name: "school",
@@ -48,7 +43,7 @@ const internQuestions = [
     }
 ]
 
-const managerQuestions = [
+const managerPrompt = [
     {
         type: "input",
         name: "office",
@@ -56,27 +51,32 @@ const managerQuestions = [
     }
 ]
 
+// This is the method for the user to interact with
+const runPrompts = () => {
+    [{
+        type: "list",
+        name: "role",
+        message: "Choose Employee Role",
+        choices: ["Engineer", "Intern", "Manager"]
+    }]
+
+    if (choices === Engineer) {
+        engineerPrompt;
+    } else if (choices === Intern) {
+        internPrompt;
+    } else {
+        managerPrompt
+    }
+}
 
 
-
-// AS A manager
-// I WANT to generate a webpage that displays my team's basic info
-// SO THAT I have quick access to their emails and GitHub profiles
-
+// AS A manager I WANT to generate a webpage that displays my team's basic info SO THAT I have quick access to their emails and GitHub profiles
 // GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
+// WHEN I am prompted for my team members and their information // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+// WHEN I click on an email address in the HTML // THEN my default email program opens and populates the TO field of the email with the address
+// WHEN I click on the GitHub username // THEN that GitHub profile opens in a new tab
+// WHEN I start the application // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+// WHEN I enter the team manager’s name, employee ID, email address, and office number // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+// WHEN I select the engineer option // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+// WHEN I select the intern option // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+// WHEN I decide to finish building my team // THEN I exit the application, and the HTML is generated
