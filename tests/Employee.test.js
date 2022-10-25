a171a0133c75b6f57af8748e505f09841ce94f28
Jax I <jaxpi85@gmail.com>
2022-10-25 15:24:31 -0400

    working on employee js and test

---
+++ tests/Employee.test.js
@@ -0,0 +1,17 @@
+const Employee = require('../lib/Employee');
+
+describe("Emplyee Class", () => {
+    describe("Initialization", () => {
+        it("should make an object with name, ID, and email properties", () => {
+//arrange
+            const employee = new Employee("Max", 1234, "MaxO@gmail.com")
+//act
+//assert
+            expect(employee).toEqual({
+                name: "Max",
+                id: "1234",
+                email: "MaxO@gmail.com"
+            })
+        })
+    })
+})
